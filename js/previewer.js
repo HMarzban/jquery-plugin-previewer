9ae34750c5d37eed18440407cc5ff73be809f939
HosseinMarzban <marzban98@gmail.com>
2018-04-15 11:52:27 +0430

    fix scrol tab, optimize memory for dom selection

--- js/previewer.js
+++ js/previewer.js
@@ -61,6 +61,7 @@
             var $click_event = is_touch_device() ? "touchstart" : "click";
             var $mouse_event = is_touch_device() ? "touchstart" : "mousedown";
             var $mouse_leave = is_touch_device() ? "touchend touchcancel" : "mouseup mouseleave";
+
             /*=========*/
             var defaults = {
                 hold_time: 1,
@@ -78,21 +79,37 @@
                     radius: " 0 0 0 6px"
                 },
                 class: "",
-                focus: true
+                focus: true,
+                dragable: false,
             };
             /*=========*/
             var timeoutId = 0;
-            var li_width = 0;
             var $title = "";
             var $loged = [];
             var $link = "";
             var $currentID = "";
+            var flag_event_bind = false;
             /*=========*/
             var $settings = $.extend({}, defaults, options);
     
             var $ID = function () {
                 return 'nd_' + Math.random().toString(36).substr(2, 9);
             };
+
+            //DOM & fn Cache 
+            var modal = {
+                $root : "",
+                $body : function(){ return this.$root.find('.nd_body'); },
+                $head : function(){ return this.$root.find('.nd_head'); },
+                _show:function(_fn){
+                    this.$root.fadeIn(_fn);
+                },
+                _hide:function(_fn){
+                   this.$root.fadeOut(_fn);
+                }
+            };
+
+
     
             $this.find('a').on('touchstart click', function (event) {
                 event.preventDefault();
@@ -113,51 +130,51 @@
                         fn_creat_newTabe($link);
                     } else {
                         //if this linke was open just open modal then switch to tab
-                        $('.modal_previewer').fadeIn();
-                        $('.modal_previewer .nd_head [data-id="' + $(_this).data('id') + '"]').trigger('click');
+                        modal._show();
+                        modal.$root.find('.nd_head [data-id="' + $(_this).data('id') + '"]').trigger('click')
                     }
                 }, $settings.hold_time);
                 /*=========*/
             }).on($mouse_leave, function (event) {
                 event.preventDefault();
                 clearTimeout(timeoutId);
-                // $('.modal_previewer').fadeOut();
+                // modal.$root.fadeOut();
             });
     
             var fn_creat_modal = function (_title) {
-                if ($('body').find('.modal_previewer').length == 0)
+                if (modal.$root.length == 0)
                     $('body').append('<div dir="' + $settings.direction +
                         '" class="modal_previewer ' + $settings.class + ' previewer_' + $settings.direction + ' " style="width:'+$settings.style.width+';height:'+$settings.style.height+'"> <div class="nd_head"> <div class="btn_prevSlide"></div> <div class="btn_nextSlide"></div> <div class="btn_close"></div> <ul></ul></div> <div class="nd_body"></div> <div class="nd_footer"></div> </div>'
                     );
-    
-                $('.modal_previewer .nd_head ul')
+
+                modal.$root = $('body .modal_previewer');
+                modal.$head().find('ul')
                     .append('<li data-id="' + $currentID + '"><span>' + $title + '</span> <b class="btn_closeTabe"></b> </li>');
     
                 if (!$settings.show_tab)
-                    $('.modal_previewer .nd_head').addClass('hide');
-    
+                   modal.$head().addClass('hide');
+
+                   fn_make_scrollHead();
+                    
             }; // fn_creat_newTabe
     
             var fn_creat_newTabe = function (_link) {
-    
-                $('.modal_previewer .nd_body > div').hide();
-    
+                //first hide all tab data
+                modal.$body().find('> div').hide();
                 var sandbox = "";
                 if ($settings.sandbox.active)
                     sandbox = 'sandbox="' + $settings.sandbox.types + '"';
-    
-                $('body')
-                    .find('.modal_previewer .nd_body')
-                    .append('<div id="' + $currentID + '"> <iframe src="' + _link + '" ' + sandbox + '></iframe> </div>')
-                    .find('#' + $currentID).fadeIn();
+                    modal.$body()
+                        .append('<div id="' + $currentID + '"> <iframe src="' + _link + '" ' + sandbox + '></iframe> </div>')
+                        .find('#' + $currentID).fadeIn();
                 //after append all necessary  element show modal
-                $('.modal_previewer').fadeIn();
-    
-                fn_creat_event_listner();
+                modal._show();
+                
+                fn_bind_event_listner();
             }; // fn_creat_newTabe
     
             var fn_init_focus_desk = function () {
-                $('.modal_previewer')
+                modal.$root
                     .mouseleave(function () {
                         $this.removeClass('modal_blur');
                     })
@@ -167,62 +184,65 @@
             };
     
             var fn_make_scrollHead = function () {
-    
-                $('.modal_previewer ul li').each(function (index, val) {
-                    li_width += $(val).innerWidth();
-                });
-    
-                var head_width = $('.modal_previewer .nd_head').innerWidth();
-                //TODO: don't forgt for ltr direction change css left to right
-    
-                console.log(li_width + "=========" + head_width);
-                if (li_width >= head_width) {
-                    $('.btn_prevSlide,.btn_nextSlide').fadeIn();
-                    $('.btn_close').addClass('active');
-    
-                } else {
-                    $('.btn_prevSlide,.btn_nextSlide').fadeOut();
-                    $('.btn_close').removeClass('active');
-                }
-            };
-            var fn_creat_event_listner = function () {
-    
-                //close modal // fadeOut
-                $('.modal_previewer .nd_head  .btn_close').on($click_event, function () {
-    
-                    $('.modal_previewer').fadeOut(function () {
-                        $this.removeClass('modal_blur');
+                var li_width = 0;
+                    $('.modal_previewer ul li').each(function (index, val) {
+                        li_width += $(val).innerWidth() + 70;
                     });
-    
-                });
-                /*======*/
-                var _direction = $settings.direction == 'rtl' ? 'left' : 'right';
+               
+
+                    var head_width = modal.$head().innerWidth();
+                    //TODO: don't forgt for ltr direction change css left to right
+        
+                    if (li_width >= head_width) {
+                        $('.btn_prevSlide,.btn_nextSlide').fadeIn();
+                        $('.btn_close').addClass('active');
+        
+                    } else {
+                        $('.btn_prevSlide,.btn_nextSlide').fadeOut();
+                        $('.btn_close').removeClass('active');
+                    } 
+            };
+
+            var fn_bind_event_listner = function () {
+
+                var _direction     = $settings.direction == 'rtl' ? 'left' : 'right';
                 var _tabSlide_prev = $settings.direction == 'rtl' ? '.btn_prevSlide' : '.btn_nextSlide';
                 var _tabSlide_next = $settings.direction == 'rtl' ? '.btn_nextSlide' : '.btn_prevSlide';
-                $(_tabSlide_prev).on($click_event, function () {
-                    var current = parseInt($('.modal_previewer ul').css(_direction));
-                    $('.modal_previewer ul').css(_direction, (current + 60) + "px");
-                });
+
+                //event handler
+                $('.modal_previewer .nd_head  .btn_close').on($click_event,fn_ev_closeBTN);// close tab handler
+                $(_tabSlide_prev).on($click_event, fn_ev_tabslide_prev);
+                $(_tabSlide_next).on($click_event, fn_ev_tabslide_next);
+                $(document).on($click_event, '.modal_previewer .nd_head ul li',fn_ev_tabTBN); // btn tab handler
                 /*======*/
-                $(_tabSlide_next).on($click_event, function () {
-                    var current = parseInt($('.modal_previewer ul').css(_direction));
+                function fn_ev_closeBTN () {
+                    
+                    modal._hide(function () {
+                        $this.removeClass('modal_blur');
+                    });
+                }
+                function fn_ev_tabslide_prev () {
+                    var current = parseInt(modal.$head().find('ul').css(_direction));
                     if (current > 0) {
                         if (current - 60 < 0) {
-                            $('.modal_previewer ul').css(_direction, '0');
+                            modal.$head().find('ul').css(_direction, '0');
                         } else {
-                            $('.modal_previewer ul').css(_direction, (current - 60) + "px");
+                            modal.$head().find('ul').css(_direction, (current - 60) + "px");
                         }
                     } else {
-                        $('.modal_previewer ul').css(_direction, '0');
+                        modal.$head().find('ul').css(_direction, '0');
                     }
-                });
-                /*======*/
-                // click tab
-                $(document).on($click_event, '.modal_previewer .nd_head ul li', function (e) {
-                    // if click close button = = otherwise change tab view
-                    if ($(e.target).hasClass('btn_closeTabe')) {
-                        var $tab_id = $(e.target).parent().data('id');
-                        var $title = $(e.target).parent().find('span').text();
+                }
+                function fn_ev_tabslide_next () {
+                    var current = parseInt(modal.$head().find('ul').css(_direction));
+                    modal.$head().find('ul').css(_direction, (current + 60) + "px");
+                }   
+                function fn_ev_tabTBN (event){
+                    
+                     // if click close button = = otherwise change tab view
+                     if ($(event.target).hasClass('btn_closeTabe')) {
+                        var $tab_id = $(event.target).parent().data('id');
+                        var $title = $(event.target).parent().find('span').text();
     
                         // remove loged Id
                         var $index = $loged.indexOf($title);
@@ -232,40 +252,45 @@
                         //TODO: find way to remove .modal_previewer then remove tab for ux & ui 
                         if ($loged.length == 0) {
                             $this.removeClass('modal_blur');
-                            $('.modal_previewer').fadeOut('fast');
+                            modal._hide('fast');
                         }
     
                         if ($(this).prev().text().length === 0) {
-                            var nextId = $(e.target).parent().next().data('id');
-                            $(".modal_previewer .nd_body").find('#' + nextId).fadeIn();
+                            var nextId = $(event.target).parent().next().data('id');
+                            modal.$body().find('#' + nextId).fadeIn();
                         } else {
-                            var prevId = $(e.target).parent().prev().data('id');
-                            $(".modal_previewer .nd_body").find('#' + prevId).fadeIn();
+                            var prevId = $(event.target).parent().prev().data('id');
+                            modal.$body().find('#' + prevId).fadeIn();
                         }
     
-                        $(".modal_previewer .nd_body").find('#' + $tab_id).remove();
-                        $(e.target).parent().remove();
-    
+                        modal.$body().find('#' + $tab_id).remove();
+                        $(event.target).parent().remove();
+
+                        fn_make_scrollHead();
                     } else {
+                        //change tab view
                         var _id = $(this).data('id');
-                        $('.modal_previewer .nd_head ul li').removeClass('active');
+                        modal.$head().find('ul li').removeClass('active');
                         $(this).addClass('active');
-                        $('.modal_previewer .nd_body > div').hide();
-                        $('.modal_previewer .nd_body > div#' + _id).show();
+                        modal.$body().find('> div').hide();
+                        modal.$body().find('> div#' + _id).show();
                     }
-    
-                }); // btn tab handler
+
+                   
+                }
+               
                 /*======*/
                 // make dragable box
-                $('.modal_previewer').nd_drags();
+                if($settings.dragable)
+                    modal.$root.nd_drags();
                 /*======*/
                 // if we are in desktop and focus is enable
                 // FIXME: cheack if is in desktop
                 if ($settings.focus)
                     fn_init_focus_desk();
+                
                 /*======*/
-                fn_make_scrollHead();
-                /*======*/
+
     
             }; // fn_creat_event_listner
         }; // $.fn.previewer 
